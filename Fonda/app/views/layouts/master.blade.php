9276d752a2b434c50750fe187a0139bc1be19cdd
israel martinez <israel.martinez.vargas@gmail.com>
2014-04-23 13:33:32 -0700

    Agrego index
    Se agrego el index y master

---
+++ Fonda/app/views/layouts/master.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="Fonda">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Fonda</title>
+        <link rel="author" href="humans.txt">
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
